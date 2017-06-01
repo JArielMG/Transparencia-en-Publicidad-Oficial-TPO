88c714a893bf969066e87a112e655107a953d63d
Kevin Martinez <n4utilius@gmail.com>
2017-06-01 12:53:59 -0500

    borrar datos previos

--- tpov1/integraadminv1/data/config.php
+++ tpov1/integraadminv1/data/config.php
@@ -1,10 +1,2 @@
 <?php
-	define('dbHOST',    base64_decode('bG9jYWxob3N0'));
-	define('dbUSER',    base64_decode('cm9vdA=='));
-	define('dbPASS',    base64_decode('azBuazR0bDBuLjFmaQ=='));
-	define('dbNAME',    base64_decode('dHBvdjE='));
-        define('IURL_ROOT', base64_decode('aHR0cDovL2xvY2FsaG9zdDo4MC9pbmFpL2ludGVncmFhZG1pbnYxLw=='));
-        define('IDIR_ROOT', base64_decode('L3Zhci93d3cvaHRtbC9pbmFpL2ludGVncmFhZG1pbnYxLw=='));
-        define('IURL_COMUN',base64_decode('aHR0cDovL2xvY2FsaG9zdDo4MC9pbmFpLw=='));
- 	define('IDIR_COMUN',base64_decode('L3Zhci93d3cvaHRtbC9pbmFpLw=='));      
 ?>
