45f73846b82d666844c9fbba3565e8a1b2cbd815
Kevin Martinez <n4utilius@gmail.com>
2017-08-29 11:56:31 -0500

    Cambiar los estilos de la gráfica

--- tpov1/tpov1/application/views/pages/Porservicio.php
+++ tpov1/tpov1/application/views/pages/Porservicio.php
@@ -220,11 +220,21 @@ function dashboard(id, fData){
             .on("mouseout",mouseout);// mouseout is defined below.
 
         //Create the frequency labels above the rectangles.
-        bars.append("text").text(function(d){ return d3.format(",")(d[1])})
-            .attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
-            .attr("y", function(d) { return y(d[1])-5; })
-            .attr("text-anchor", "middle");
+        /**/
         
+        bars.append("text")
+            .text(function(d){ return d3.format(",")(d[1])})
+            .style("font-size", "8px")
+            //.attr("x", function(d) { return x(d[0])+x.rangeBand()/2; })
+            .attr("x", function(d) { return ( x(d[0]) + (x.rangeBand()) * 0.4); })
+            //.attr("y", function(d) { return y(d[1])-5; })
+            .attr("y", function(d) { return (y(d[1]) - 5); })
+            .attr("rotate", function(d, i) {
+                //return "translate(" + ( x(d[0]) + (x.rangeBand()) * 0.6) + "," + (y(d[1]) - 30) + ") rotate(-90)";
+                return "rotate(-90)";
+              })
+            .attr("text-anchor", "middle");
+        /**/
         function mouseover(d){  // utility function to be called on mouseover.
             // filter for selected state.
             var st = fData.filter(function(s){ return s.State == d[0];})[0],
@@ -258,7 +268,7 @@ function dashboard(id, fData){
 
             // transition the frequency labels location and change value.
             bars.select("text").transition().duration(500)
-                .text(function(d){ return d3.format(",")(d[1])})
+                //.text(function(d){ return d3.format(",")(d[1])})
                 .attr("y", function(d) {return y(d[1])-5; });            
         }        
         return hG;
@@ -333,7 +343,7 @@ function dashboard(id, fData){
             this._current = i(0);
             return function(t) { return arc(i(t));    };
         }    
-        $(".pie").prependTo(".histo")
+        $(".pie").insertBefore(".histo")
         return pC;
     }
     
@@ -356,7 +366,7 @@ function dashboard(id, fData){
         // create the first column for each segment.
         tr.append("td").append("svg").attr("width", '16').attr("height", '16').append("rect")
             .attr("width", '16').attr("height", '16')
-			.attr("fill",function(d){ return segColor(d.type); });
+      .attr("fill",function(d){ return segColor(d.type); });
             
         // create the second column for each segment.
         tr.append("td").text(function(d){ return mytitles[basetitles.indexOf(d.type)]; });
@@ -439,11 +449,11 @@ function dashboard(id, fData){
     $xcrud->label('factura','Factura')
           ->label('proveedor','Proveedor')
           ->label('fecha_erogacion','Fecha')
-	  ->label('nombre_servicio_clasificacion','Clasificación')
-	  ->label('nombre_servicio_categoria','Categoría')
-	  ->label('nombre_servicio_subcategoria','Subcategoría')
-	  ->label('nombre_campana_aviso','Campaña o aviso')
-	  ->label('monto_servicio','Monto gastado');    
+    ->label('nombre_servicio_clasificacion','Clasificación')
+    ->label('nombre_servicio_categoria','Categoría')
+    ->label('nombre_servicio_subcategoria','Subcategoría')
+    ->label('nombre_campana_aviso','Campaña o aviso')
+    ->label('monto_servicio','Monto gastado');    
     $xcrud->sum('monto_servicio');        
     $xcrud->column_class('fecha_erogacion,monto_servicio', 'align-center');
     $xcrud->change_type('monto_servicio',  'price', '0', array('prefix'=>'$ ', 'decimals'=>2));
