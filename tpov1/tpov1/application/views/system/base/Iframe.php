c28a05ef04683c98e045beae84c9c2845e741569
Kevin Martínez <n4utilius@gmail.com>
2017-10-06 16:17:31 -0500

    Quitar scroll en iframes

--- tpov1/tpov1/application/views/system/base/Iframe.php
+++ tpov1/tpov1/application/views/system/base/Iframe.php
@@ -6,12 +6,12 @@ die();
 <?php
    if ( $ScreenTarget == "Sys_Screen?v=Inicio&g=pages/") {
 ?>
-<iframe src="<?php echo $ScreenTarget; ?>" style="width:100%;" frameborder="0" id="myframe"
+<iframe scrolling=no src="<?php echo $ScreenTarget; ?>" style="width:100%;" frameborder="0" id="myframe"
         onload="this.style.height = '950px';"> </iframe>
 <?php
   } else {
 ?>
-<iframe src="<?php echo $ScreenTarget; ?>" style="width:100%;" frameborder="0" id="myframe"
+<iframe scrolling=no src="<?php echo $ScreenTarget; ?>" style="width:100%;" frameborder="0" id="myframe"
         onload="this.style.height = this.contentWindow.document.body.scrollHeight + 22 + 'px';"> </iframe>
 <?php
    }
