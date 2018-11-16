e97e3a10101f66f60b711144972c7dbb106bc351
JArielMG <ariel.morales.glez@gmail.com>
2018-11-15 18:06:53 -0600

    Update MenuA.php

--- tpov1/tpoadminv1/application/views/system/base/submenus/MenuA.php
+++ tpov1/tpoadminv1/application/views/system/base/submenus/MenuA.php
@@ -11,31 +11,6 @@
          .menu-btn {
             display:  none;
          }
-/*
-
-      @media (max-width: 1000px) {
-         .menu {
-            display: none;
-         }
-         .menu2 {
-            display: none;
-         }
-         .menu-btn {
-            display:  block;
-         }
-      }
-      @media (min-width: 1000px) {
-         .menu {
-            display: block;
-         }
-         .menu2 {
-            display: block;
-         }
-         .menu-btn {
-            display:  none;
-         }
-      }
-*/
    </style>
 <?php
    $user_id = getD3D("user_id");
@@ -47,37 +22,37 @@
       <br>	        
       <div class="menu" style="margin-top:15px;height:77px;">
          <ul>
-            <li style="width:135px;">
+            <li style="width:143px;">
                <a href="Sys_Hub?v=Facturas&g=pages"
                   <?php if ($liga === 'pages/Facturas')  { echo 'id="primero"'; } ?> >Facturas
                </a>
             </li>
-            <li style="width:135px;">
+            <li style="width:143px;">
                <a href="Sys_Hub?v=Ordenes&g=pages"  
                   <?php if ($liga === 'pages/Ordenes') { echo 'id="primero"'; } ?> >Órdenes de compra
                </a>
             </li>
-            <li style="width:135px;">
+            <li style="width:143px;">
                <a href="Sys_Hub?v=Contratos&g=pages"  
                   <?php if ($liga === 'pages/Contratos') { echo 'id="primero"'; } ?> >Contratos
                </a>
             </li>
-            <li style="width:135px;">
-               <a href="Sys_Hub?v=Campanas&g=pages"  style="margin-top:-10px;"
+            <li style="width:143px;">
+               <a href="Sys_Hub?v=Campanas&g=pages"
                   <?php if ($liga === 'pages/Campanas') { echo 'id="primero"'; } ?> >Campañas y <br>avisos institucionales
                </a>
             </li>
-            <li style="width:135px;">
+            <li style="width:143px;">
                <a href="Sys_Hub?v=Presupuestos&g=pages"  
                   <?php if ($liga === 'pages/Presupuestos') { echo 'id="primero"'; } ?> >Presupuestos
                </a>
             </li>
-            <li style="width:135px;">
+            <li style="width:143px;">
                <a href="Sys_Hub?v=Proveedores&g=pages"
                   <?php if ($liga === 'pages/Proveedores')  { echo 'id="primero"'; } ?> > Proveedores
                </a>
             </li>
-            <li style="width:135px;">
+            <li style="width:142px;">
                <a href="Sec_Login" >Salir</a>
             </li>
          </ul>
